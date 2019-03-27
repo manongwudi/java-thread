44c2e2be6158091873b7e2f8aae6de58d7d04a04
jiangqiao <jiangqiao@mobike.com>
2019-03-27 12:02:30 +0800

    测试

---
+++ src/main/java/cn/wudimanong/thread/DaemonThread01.java
@@ -0,0 +1,25 @@
+package cn.wudimanong.thread;
+
+/**
+ * @author Joe
+ */
+public class DaemonThread01 {
+
+    public static void main(String args[]) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+        thread.setDaemon(true);//将thread设置为守护线程
+        thread.start();
+        Thread.sleep(2000);
+        System.out.println("Main thread finished lifecycle.");
+    }
+
+}
